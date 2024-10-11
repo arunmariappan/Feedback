ba00e18d73ed0da58f34a666f2857f34160fdc10
Arun Mariappan Karunanithi <arunjai@gmail.com>
2024-10-11 18:25:11 +0530

    Textarea counter changes

--- CustomerFeedbackApp/wwwroot/js/site.js
+++ CustomerFeedbackApp/wwwroot/js/site.js
@@ -87,4 +87,43 @@
             $(element).parents(".col-sm-10").addClass("has-success").removeClass("has-error");
         }
     });
+
+    // textarea counter
+    setTimeout(function () {
+        $("#feedbackmessage").on("keyup", function (e) {
+            console.log("aasafafs");
+            var characterCount = $(this).val().length,
+                current = $('#current'),
+                maximum = $('#maximum'),
+                theCount = $('#the-count');
+
+            current.text(characterCount);
+
+            /*This isn't entirely necessary, just playin around*/
+            if (characterCount < 70) {
+                current.css('color', '#666');
+            }
+            if (characterCount > 70 && characterCount < 90) {
+                current.css('color', '#6d5555');
+            }
+            if (characterCount > 90 && characterCount < 100) {
+                current.css('color', '#793535');
+            }
+            if (characterCount > 100 && characterCount < 120) {
+                current.css('color', '#841c1c');
+            }
+            if (characterCount > 120 && characterCount < 139) {
+                current.css('color', '#8f0001');
+            }
+
+            if (characterCount >= 140) {
+                maximum.css('color', '#8f0001');
+                current.css('color', '#8f0001');
+                theCount.css('font-weight', 'bold');
+            } else {
+                maximum.css('color', '#666');
+                theCount.css('font-weight', 'normal');
+            }
+        });
+    }, 500);    
 });
